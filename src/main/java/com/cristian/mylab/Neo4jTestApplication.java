cb2500deb59f5ddbc020679cda7aef6837d96b9f
cristianprofile <cristianprofile@gmail.com>
2015-04-18 14:52:11 +0200

    Add test class
    Test  class with simple query to my little graph

--- src/main/java/com/cristian/mylab/Neo4jTestApplication.java
+++ src/main/java/com/cristian/mylab/Neo4jTestApplication.java
@@ -1,73 +1,17 @@
 package com.cristian.mylab;
 
-import java.io.File;
 import java.io.IOException;
 
-import org.neo4j.kernel.impl.util.FileUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.neo4j.core.GraphDatabase;
-import org.neo4j.graphdb.Transaction;
-
-import com.cristian.mylab.entity.Person;
-import com.cristian.mylab.repository.PersonRepository;
 
 @SpringBootApplication
 public class Neo4jTestApplication {
 	
-	@Autowired PersonRepository personRepository;
-
-	@Autowired GraphDatabase graphDatabase;
-
-
-	
-	
-	public void run(String... args) throws Exception {
-
-		Person carlos = new Person("Carlos");
-		Person cristian = new Person("Cristian");
-		Person pepe = new Person("Pepe");
-
-		
-		Transaction tx = graphDatabase.beginTx();
-		try {
-			personRepository.save(carlos);
-			personRepository.save(cristian);
-			personRepository.save(pepe);
-
-			// if cristian works with pepe and carlos
-			
-			cristian = personRepository.findByName(cristian.name);
-			cristian.worksWith(carlos);
-			cristian.worksWith(pepe);
-			personRepository.save(cristian);
-
-			//carlos works with pepe
-			carlos = personRepository.findByName(carlos.name);
-			carlos.worksWith(pepe);
-				
-			
-			personRepository.save(pepe);
-		
-
-
-			Iterable<Person> findByTeammatesName = personRepository.findByTeammatesName("Cristian");
-	
-			System.out.println("Looking up who works with Greg...");
-			for (Person person : findByTeammatesName) {
-				System.out.println(person.name + " works with Greg.");
-			}
-
-			tx.success();
-		} finally {
-			tx.close();
-		}
-	}
 	
 	
     public static void main(String[] args) throws IOException {
-    	FileUtils.deleteRecursively(new File("cristianNeo4jTest.db"));
+    	
         SpringApplication.run(Neo4jTestApplication.class, args);
     }
 }
--- src/test/java/demo/Neo4jTestApplicationTests.java
+++ src/test/java/demo/Neo4jTestApplicationTests.java
@@ -1,18 +1,72 @@
 package demo;
 
+import java.io.IOException;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.neo4j.graphdb.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.data.neo4j.core.GraphDatabase;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 import com.cristian.mylab.Neo4jTestApplication;
+import com.cristian.mylab.entity.Person;
+import com.cristian.mylab.repository.PersonRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Neo4jTestApplication.class)
 public class Neo4jTestApplicationTests {
 
+	@Autowired PersonRepository personRepository;
+
+	@Autowired GraphDatabase graphDatabase;
+	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws IOException {
+		Person carlos = new Person("Carlos");
+		Person cristian = new Person("Cristian");
+		Person pepe = new Person("Pepe");
+
+		
+		Transaction tx = graphDatabase.beginTx();
+		try {
+			personRepository.save(carlos);
+			personRepository.save(cristian);
+			personRepository.save(pepe);
+
+			// if cristian works with pepe and carlos
+			
+			cristian = personRepository.findByName(cristian.name);
+			cristian.worksWith(carlos);
+			cristian.worksWith(pepe);
+			personRepository.save(cristian);
+
+			//carlos works with pepe
+			carlos = personRepository.findByName(carlos.name);
+			carlos.worksWith(pepe);
+				
+			
+			personRepository.save(pepe);
+		
+
+
+			Iterable<Person> findByTeammatesName = personRepository.findByTeammatesName("Cristian");
+	
+			logger.info("Looking up who works with Cristian...");
+			for (Person person : findByTeammatesName) {
+				logger.info("{} works with Greg.",person.name);
+			}
+
+			tx.success();
+		} finally {
+			tx.close();
+		}
 	}
+	
+	
 
 }
